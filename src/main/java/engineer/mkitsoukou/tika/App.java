16c143d914b440270f8205b5135bac3b3f58596a
hoddieBoy <emilemannekitsoukou@gmail.com>
2025-06-14 14:04:07 +0200

    feat(init): initialize Maven project with sample app
    Sets up a basic Maven project structure including a simple application and unit test scaffold to support future development and testing.
    
    Provides a foundation for building and testing Java services with Maven.

---
+++ src/main/java/engineer/mkitsoukou/tika/App.java
@@ -0,0 +1,13 @@
+package engineer.mkitsoukou.tika;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
