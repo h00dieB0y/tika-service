bb8522682389b44055f2c150fcb339d7457fc412
hoddieBoy <emilemannekitsoukou@gmail.com>
2025-07-06 09:21:45 +0200

    feat(shared): add ClockPort interface for time management
    This change introduces the ClockPort interface, which provides methods
    to retrieve the current time and the system's time zone. It includes
    a default implementation for easy access to the current time and
    zone, facilitating time-related operations across the application.

---
+++ tika-application/src/main/java/engineer/mkistoukou/tika/application/shared/ClockPort.java
@@ -0,0 +1,21 @@
+package engineer.mkistoukou.tika.application.shared;
+
+import java.time.Instant;
+import java.time.ZoneId;
+
+public interface ClockPort {
+  Instant now();
+  ZoneId zoneId();
+
+  ClockPort SYSTEM = new ClockPort() {
+    @Override
+    public Instant now() {
+      return Instant.now();
+    }
+
+    @Override
+    public ZoneId zoneId() {
+      return ZoneId.systemDefault();
+    }
+  };
+}
