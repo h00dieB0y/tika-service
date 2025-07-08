4298a27f186fe071a287c57c628f1dbdab2e54c4
hoddieBoy <emilemannekitsoukou@gmail.com>
2025-07-08 08:19:31 +0200

    refactor(application): remove unused import in JwtIssuerPort
    This change cleans up the JwtIssuerPort interface by removing an
    unused import statement for the User entity, improving code clarity
    and maintainability.

--- tika-application/src/main/java/engineer/mkitsoukou/tika/application/auth/port/out/JwtIssuerPort.java
+++ tika-application/src/main/java/engineer/mkitsoukou/tika/application/auth/port/out/JwtIssuerPort.java
@@ -2,8 +2,6 @@ package engineer.mkitsoukou.tika.application.auth.port.out;
 
 import engineer.mkitsoukou.tika.application.auth.dto.AuthTokensDto;
 import engineer.mkitsoukou.tika.application.auth.model.AuthSubject;
-import engineer.mkitsoukou.tika.domain.model.entity.User;
-
 import java.time.Instant;
 
 public interface JwtIssuerPort {
